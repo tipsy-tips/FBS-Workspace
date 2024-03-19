ddfac17f1538ae81bd6bd595ace7c0d1b782ecc4
tipsy-tips <itaikichi@gmail.com>
2024-03-19 15:17:03 +0800

    update to tailwind-starbucks

--- Tailwiwnd-Projects/tailwind-starbucks/dist/js/script.js
+++ Tailwiwnd-Projects/tailwind-starbucks/dist/js/script.js
@@ -1,6 +1,40 @@
+const starNav = document.querySelectorAll(".coffee__nav__link");
+const starContent = document.querySelectorAll(".coffee__item");
+
+starNav.forEach((star) => {
+  star.addEventListener("click", () => {
+    removeActiveStar();
+    star.classList.add("active");
+    
+    const activeContent = document.querySelector(`#${star.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  });
+});
+
+function removeActiveStar() {
+  starNav.forEach((star) => {
+    star.classList.remove("active");
+  });
+}
+
+function removeActiveContent() {
+  starContent.forEach((content) => {
+    content.classList.remove("active");
+  });
+}
+
 const burgerbtn = document.querySelector(".burger__menu");
 const headerA = document.querySelector(".header__action");
 burgerbtn.addEventListener("click", () => {
   burgerbtn.classList.toggle("open");
   headerA.classList.toggle("open");
+});
+
+const links = document.querySelectorAll(".links__item h4");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    link.querySelector("i").classList.toggle("open");
+    link.nextElementSibling.classList.toggle("open");
+  });
 });
--- Tailwiwnd-Projects/tailwind-starbucks/tailwind.config.js
+++ Tailwiwnd-Projects/tailwind-starbucks/tailwind.config.js
@@ -2,10 +2,15 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
+      backgroundImage: {
+        starBanner: "url(../dist/img/rewards/xl-hero-desktop_2021.png)",
+        starBannerM: "url(../dist/img/rewards/hero-mobile_2021.webp)",
+      },
       colors: {
         dark: "#2b2b2b",
         light: "#ffffff",
         primary: "#006241",
+        secondary: "#d0eae4",
       },
     },
   },
