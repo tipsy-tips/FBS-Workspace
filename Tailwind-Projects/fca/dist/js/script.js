3a9b8ce4ace328659cca66179cf671592c8c0e22
tipsy-tips <itaikichi@gmail.com>
2024-03-21 14:02:57 +0800

    updated

--- Tailwind-Projects/fca/dist/js/script.js
+++ Tailwind-Projects/fca/dist/js/script.js
@@ -4,3 +4,18 @@ dropdown.forEach((dropdowns) => {
     dropdowns.classList.toggle("active");
   });
 });
+
+const burgerbtn = document.querySelector(".burger__menu");
+const headerA = document.querySelector(".header__action");
+burgerbtn.addEventListener("click", () => {
+  burgerbtn.classList.toggle("open");
+  headerA.classList.toggle("open");
+});
+
+const links = document.querySelectorAll(".links__item h4");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    link.querySelector("i").classList.toggle("open");
+    link.nextElementSibling.classList.toggle("open");
+  });
+});
--- Tailwind-Projects/fca/tailwind.config.js
+++ Tailwind-Projects/fca/tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+import {
+  fluidExtractor,
+  fluidCorePlugins,
+  defaultThemeScreensInRems,
+  defaultThemeFontSizeInRems,
+} from "fluid-tailwind";
+
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: {
+    files: ["./dist/**/*.{html,js}"],
+    extract: fluidExtractor(),
+  },
   theme: {
     extend: {
+      fontSize: defaultThemeFontSizeInRems,
+      screens: defaultThemeScreensInRems,
       backgroundImage: {},
       colors: {
         dark: "#2b2b2b",
         light: "#ffffff",
         primary: "#008542",
-      },
-      listStyleType: {
-        square: "square",
+        accent: "#28941e",
       },
     },
   },
-  plugins: [],
+  plugins: [fluidCorePlugins],
 };
