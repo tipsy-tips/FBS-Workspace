7597ec246d752075e14846d74a5bd77fb39e3b79
tipsy-tips <itaikichi@gmail.com>
2024-03-22 07:50:11 +0800

    footer updated

--- Tailwind-Projects/fca/dist/js/script.js
+++ Tailwind-Projects/fca/dist/js/script.js
@@ -1,3 +1,26 @@
+const menuItems = document.querySelectorAll("#menu li");
+const contentContainers = document.querySelectorAll('[class^="faq-menu"]');
+
+menuItems.forEach((menuItem, index) => {
+  menuItem.addEventListener("click", () => {
+    // Hide all content containers
+    contentContainers.forEach((container) => {
+      container.style.display = "none";
+    });
+
+    // Display the content container corresponding to the clicked menu item
+    contentContainers[index].style.display = "block";
+
+    // Remove active-menu class from all menu items
+    menuItems.forEach((item) => {
+      item.classList.remove("active-menu");
+    });
+
+    // Add active-menu class to the clicked menu item
+    menuItem.classList.add("active-menu");
+  });
+});
+
 const dropdown = document.querySelectorAll(".dropdownItem");
 dropdown.forEach((dropdowns) => {
   dropdowns.addEventListener("click", () => {
