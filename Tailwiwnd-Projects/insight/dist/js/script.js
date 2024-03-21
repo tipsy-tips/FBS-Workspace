cbb19916b20bf6d155230cd44afd60bd115f04d0
tipsy-tips <itaikichi@gmail.com>
2024-03-21 08:49:01 +0800

    updates to nav bar responsiveness

--- Tailwiwnd-Projects/insight/dist/js/script.js
+++ Tailwiwnd-Projects/insight/dist/js/script.js
@@ -1,3 +1,22 @@
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 const burgerbtn = document.querySelector(".burger__menu");
 const headerA = document.querySelector(".header__action");
 burgerbtn.addEventListener("click", () => {
