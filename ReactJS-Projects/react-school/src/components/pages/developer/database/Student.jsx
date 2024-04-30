159af497427eaa696b0bfebf83a53bb999191a51
tipsy-tips <itaikichi@gmail.com>
2024-04-30 14:00:22 +0800

    initial commit

---
+++ ReactJS-Projects/react-school/src/components/pages/developer/database/Student.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navigation from "../../../partials/Navigation";
+import Header from "../../../partials/Header";
+
+const Student = () => {
+  return (
+    <section className="flex">
+      <Navigation />
+      <main>
+        <Header />
+        <h2>Database</h2>
+        <p>TABLE HERE</p>
+      </main>
+    </section>
+  );
+};
+
+export default Student;
